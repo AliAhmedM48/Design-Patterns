f868453dffe9e99805e20d3cfe50eea2b391260d
Ali Ahmed <63220520+AliAhmedM48@users.noreply.github.com>
2024-10-01 16:31:58 +0300

    Logging System (C# Singleton)

---
+++ Design_Patterns.Singleton/Logger.cs
@@ -0,0 +1,32 @@
+﻿namespace Design_Patterns.Singleton;
+
+internal class Logger
+{
+    private static Logger? instance = null;
+    private string logFilePath = "app.log";
+
+    private Logger()
+    {
+        if (!File.Exists(logFilePath))
+        {
+            File.Create(logFilePath).Close();
+        }
+    }
+
+    public static Logger GetInstance()
+    {
+        if (instance is null)
+        {
+            instance = new Logger();
+        }
+        return instance;
+    }
+
+    public void LogMessage(string message)
+    {
+        using (StreamWriter writer = File.AppendText(logFilePath))
+        {
+            writer.WriteLine($"{DateTime.Now}: {message}");
+        }
+    }
+}
